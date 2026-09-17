71f5c09d555efd5ef604281243933ea080b60573
Abrar <abrara276@gmail.com>
2026-09-17 18:25:34 +0600

    Finished fixing the NavBar for Shop and started SearchBar for the Header

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,8 +1,11 @@
+import { Search } from 'lucide-react'
 import React from 'react'
 
 const SearchBar = () => {
   return (
-    <div>SearchBar</div>
+    <div>
+        <Search className= "w-5 h-5 hover:text-shop_light_green" />
+    </div>
   )
 }
 
