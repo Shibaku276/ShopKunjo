2f02149d734526a3c0123b34385a781f680d4aa4
Abrar <abrara276@gmail.com>
2026-09-22 13:42:08 +0600

    Finished the Footer and setup of Sanity.io for Content Management, still need to make the border on Footer be fixed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,11 @@ import Container from './Container'
 import FooterTop from './FooterTop'
 import Logo from "./Logo"
 import SocialMedia from './SocialMedia'
-import { SubText } from './ui/text'
+import { SubText, SubTitle } from './ui/text'
+import { categoriesData, quickLinksData } from '@/constants/data'
+import Link from 'next/link'
+import { Input } from './ui/input'
+import { Button } from './ui/button'
 
 
 const Footer = () => {
@@ -20,23 +24,46 @@ const Footer = () => {
               <SocialMedia className = "text-darkColor/60" iconClassName = "border-darkColor/60 hover:border-shop_light_green hover:text-shop_light_green" tooltipClassName='bg-darkColor text-white' />
             </div>
             <div>
-
+              <SubText>Quick Links</SubText>
+              <ul className="space-y-3 mt-4 ">
+                {quickLinksData?.map((item) => (
+                  <li key= {item?.title}>
+                    <Link href={item?.href} className = "hover:text-shop_light_green hoverEffect font-medium">
+                      {item?.title}
+                    </Link>    
+                  </li>
+                ))}
+              </ul>
             </div>
             <div>
-
+              <SubText>Categories</SubText>
+              <ul className="space-y-3 mt-4 ">
+                {categoriesData?.map((item) => (
+                  <li key= {item?.title}>
+                    <Link href={`/ctaegory/${item?.href}`} className = "hover:text-shop_light_green hoverEffect font-medium">
+                      {item?.title}
+                    </Link>    
+                  </li>
+                ))}
+              </ul>
             </div>
-            <div>
-
+            <div className='space-y-4'>
+                <SubTitle>Newsletter</SubTitle>
+                <SubText>Subscribe to our Newsletter to receive the latest updates and exclusive offers.</SubText>
+                <form className='space-y-3'>
+                  <Input placeholder='Enter your Email' type= 'email' required />
+                  <Button className='w-full'>Subscribe</Button>
+                </form>
             </div>
           </div>
+          <div className='py-6 border-t text-center text-sm text-gray-600'>
+              <div>
+                &copy; {new Date().getFullYear()} <Logo className='text-sm' />. All rights reserved.
+              </div>
+            </div>
         </Container>
     </footer>
   )
 }
-
-
-
-
-
 
 export default Footer
--- components/ui/text.tsx
+++ components/ui/text.tsx
@@ -9,8 +9,18 @@ const Title = ({children, className}:{children:React.ReactNode; className?: stri
     )
 }
 
+const SubTitle = ({children, className}:{children:React.ReactNode; className?: string;}) => {
+    return (
+        <h3 className = {cn("font-semibold text-gray-900 font-sans", className)}>
+            {children}
+        </h3>
+    )
+}
+
 const SubText = ({children, className}:{children:React.ReactNode; className?: string;}) => {
     return <p className = {cn('text-gray-600 text-sm', className)}>{children}</p>
 }
 
-export {Title, SubText};
+
+
+export {Title, SubText, SubTitle};
