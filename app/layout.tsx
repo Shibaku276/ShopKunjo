e9fc92f7b063113717b0ed3c85b451b16f1dbfc2
Abrar <abrara276@gmail.com>
2026-09-22 17:04:56 +0600

    Finished setting up the Content Management Server for Sanity, set up two layouts for Sanity and the Website, started on the Schema for the products

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,51 +1,9 @@
-import type { Metadata } from "next";
-//import { Geist, Geist_Mono } from "next/font/google";
-import {ClerkProvider} from "@clerk/nextjs";
-
-import "./globals.css";
-import { Geist } from "next/font/google";
-import { cn } from "@/lib/utils";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import Container from "@/components/Container";
-
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
-
-
-/*
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-*/
-
-export const metadata: Metadata = {
-  title: {
-    template: "%s | ShopKunjo Online Store",
-    default: "ShopKunjo Online Store",
-  },
-  description: "ShopKunjo is an online store that sells a variety of products, including clothing, accessories, and home goods.",
+const RootLayout = ({ children }: Readonly <{ children: React.ReactNode }>) => {
+  return (
+    <html lang="en">
+      <body className="font-poppins antialiased">{children}</body>
+    </html>
+  );
 };
 
-export default function RootLayout({ children }: LayoutProps<"/">) {
-  return (
-    <ClerkProvider>
-      <html lang="en" className={cn("font-sans", geist.variable)}>
-        <body className="font-poppins antialiased">
-          <div className = "flex flex-col min-h-screen">
-            <Header/>
-            <main className = "flex-1">
-              {children}
-            </main>
-          <Footer/>
-          </div>
-        </body>
-      </html>
-    </ClerkProvider>
-  );
-}
+export default RootLayout;
